426372724f974e6e6dbf2150e6565abe1f96fd43
rutkovsky <rus.rutkovsky@yandex.ru>
2022-11-14 21:58:33 +0300

    03-props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from "./Map";
+import {Fragment as Frag} from "./Fragment";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Frag/>
+            <input/>
+            <Rating/>
+            <Map/>
+        </div>
+    );
 }
 
+function RatingTitle() {
+    return (
+        <div>
+            <h3>Top100</h3>
+        </div>
+    );
+}function RatingBody() {
+    return (
+        <div>
+            <ul>1</ul>
+            <ul>2</ul>
+            <ul>3</ul>
+        </div>
+    );
+}
+function Rating() {
+    return (
+        <div>
+            <RatingTitle/>
+            <RatingBody/>
+        </div>
+        )
+}
 export default App;
